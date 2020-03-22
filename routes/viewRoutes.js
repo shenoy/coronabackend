d4c30a8d5454bf21d680c9ba8088893e859ca606
Rajesh Shenoy <rajeshtshenoy@gmail.com>
2020-03-22 03:08:41 +0000

    protected routes

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,12 +5,12 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 router.use(authController.isLoggedIn);
-router.get('/', viewController.getOverview);
+router.get('/', authController.protect, viewController.getOverview);
 router.get('/case/:id', viewController.getDetail);
-router.get('/total', viewController.getTotal);
-router.get('/deaths', viewController.getDeaths);
+router.get('/total', authController.protect, viewController.getTotal);
+router.get('/deaths', authController.protect, viewController.getDeaths);
 router.get('/signup', viewController.getSignupForm);
 router.get('/login', viewController.getLoginForm);
-router.get('/chat', viewController.getChat);
+router.get('/chat', authController.protect, viewController.getChat);
 
 module.exports = router;
