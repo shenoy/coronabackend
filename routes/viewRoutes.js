3a37798d7ad752aed74cc98171bc82611104b134
Rajesh Shenoy <rajeshtshenoy@gmail.com>
2020-03-22 03:17:59 +0000

    chat auth

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -5,10 +5,10 @@ const authController = require('../controllers/authController');
 const router = express.Router();
 
 router.use(authController.isLoggedIn);
-router.get('/', authController.protect, viewController.getOverview);
+router.get('/', viewController.getOverview);
 router.get('/case/:id', viewController.getDetail);
-router.get('/total', authController.protect, viewController.getTotal);
-router.get('/deaths', authController.protect, viewController.getDeaths);
+router.get('/total', viewController.getTotal);
+router.get('/deaths', viewController.getDeaths);
 router.get('/signup', viewController.getSignupForm);
 router.get('/login', viewController.getLoginForm);
 router.get('/chat', authController.protect, viewController.getChat);
