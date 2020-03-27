5d1512e45a35868873ab5526d5f5d501d6cf22b6
Rajesh Shenoy <rajeshtshenoy@gmail.com>
2020-03-27 16:32:28 +0000

    1632

--- public/js/deathChart.js
+++ public/js/deathChart.js
@@ -17,14 +17,14 @@ const deathChart = new Chart(myChart, {
     ],
     datasets: [
       {
-        label: 'Deaths',
-        data: [155,63,67,23,29,53,24,25,10,28],
-        backgroundColor: 'red'
+        label: 'Infections',
+        data: [3919, 1129, 1636, 453, 592, 837, 891, 1059, 275, 921],
+        backgroundColor: 'pink'
       },
       {
-        label: 'Infections',
-        data: [3247,876,1296,397,480,703,698,894,241,741],
-        backgroundColor: 'pink'
+        label: 'Deaths',
+        data: [155, 63, 67, 23, 29, 53, 24, 33, 13, 34],
+        backgroundColor: 'red'
       }
     ]
   },
